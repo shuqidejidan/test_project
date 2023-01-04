30230f30e245482b6bd730cb18dcf39b4f910e0f
huangjingtao <>
2023-01-04 15:31:30 +0800

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	//处理路由
 	engine.GET("/", func(context *gin.Context) {
 		//以字符串格式返回
-		context.String(200, "这就是一个普通的字符串----edit")
+		context.String(200, "这就是一个普通的字符串----edit哈哈哈哈")
 	})
 	//运行服务器,默认监听8080端口
 	engine.Run(":8081")
